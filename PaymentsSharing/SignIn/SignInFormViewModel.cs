06e3743ca580389ddb4d959724ece8e07af5021b
Daniel Kowalski <danielk.biznes@gmail.com>
2024-01-26 15:48:12 +0100

    Add login keypress event and refactor UI layout
    This commit includes the addition of a keypress (Enter key) event in the login form to improve user experience. The layout of the app has also been modified, making it dependent on the login state and there are changes in the AppBar rendering as well. Several miscellaneous refactors improve code readability and efficiency.

--- PaymentsSharing/SignIn/SignInFormViewModel.cs
+++ PaymentsSharing/SignIn/SignInFormViewModel.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using PaymentsSharing.Persons;
 
 namespace PaymentsSharing.SignIn;
@@ -9,6 +10,14 @@ internal class SignInFormViewModel(Users users, IPublisher mediator, NavigationM
     public string Username { get; set; } = "";
     public string Password { get; set; } = "";
     public bool WrongCredentials { get; private set; }
+    
+    public void OnKeyDown(KeyboardEventArgs eventArgs)
+    {
+        if (eventArgs.Key == "Enter")
+        {
+            SignIn();
+        }
+    }
     
     public void SignIn()
     {
