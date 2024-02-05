0e8925298a92a3c34c257d5ad06f3dc0c4a405db
Daniel Kowalski <danielk.biznes@gmail.com>
2024-02-05 21:21:49 +0100

    Add recalculating refunds after payments was added

--- PaymentsSharing/Payments/PaymentAddedHandler.cs
+++ PaymentsSharing/Payments/PaymentAddedHandler.cs
@@ -1,11 +1,13 @@
 using MediatR;
+using PaymentsSharing.Refunds;
 
 namespace PaymentsSharing.Payments;
 
-internal class PaymentAddedHandler : INotificationHandler<PaymentAdded>
+internal class PaymentAddedHandler(Refunds.Refunds refunds) : INotificationHandler<PaymentAdded>
 {
     public Task Handle(PaymentAdded notification, CancellationToken cancellationToken)
     {
+        refunds.Recalculate();
         return Task.CompletedTask;
     }
 }
