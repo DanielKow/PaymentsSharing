0eebf2ba647914fdaf29a15319315516b94a3856
Daniel Kowalski <danielk.biznes@gmail.com>
2024-06-27 20:37:01 +0200

    Add total payment and refactor UI
    Implemented a total payment summary in the PaymentsListViewModel. Subsequently, refactored the PaymentsList.razor to use MudSimpleTable instead of MudTable. This update included adding a row in the table to show the total payment sum.

--- PaymentsSharing/Payments/PaymentsListViewModel.cs
+++ PaymentsSharing/Payments/PaymentsListViewModel.cs
@@ -8,11 +8,14 @@ internal class PaymentsListViewModel(Payments payments)
     
     public IEnumerable<Payment> Payments =>
         payments.FromMonth(MonthAndYear).OrderBy(payment => payment.CreatedAt);
-    
+
+    public string Total => Payments.Sum(payment => payment.Amount + (payment.AmountForMeat ?? 0)).ToString("0 PLN");
+
     public string HowMuch(Payment payment) =>
         $"{payment.Amount:0 PLN} {payment.AmountForMeat?.ToString("+ 0 PLN") ?? ""}";
 
     public string When(Payment payment) => payment.CreatedAt.ToString("dd.MM.yyyy");
 
     public string ForWhat(Payment payment) => payment.Description;
+
 }
