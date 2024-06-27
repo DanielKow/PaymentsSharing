1857ea243c5929f175c7801d09ba0c7b4841389f
Daniel Kowalski <danielk.biznes@gmail.com>
2024-06-27 20:31:20 +0200

    Refactor refund calculations and presentation
    Restructured the refund logic to calculate totals and individual amounts for refunds more effectively. The Refunds view has been simplified to present refund information in a more straightforward way. Superfluous components and methods were removed in favor of more condensed and efficient code.

--- PaymentsSharing/Refunds/Refunds.cs
+++ PaymentsSharing/Refunds/Refunds.cs
@@ -7,11 +7,32 @@ internal class Refunds(Payments.Payments payments)
 {
     public IEnumerable<Refund> FromMonth(MonthAndYear monthAndYear)
     {
-        List<Refund> refunds = [];
+        decimal totalAmount = 0;
+        decimal totalAmountForMeat = 0;
         
         foreach (Payment payment in payments.FromMonth(monthAndYear))
         {
-            
+            totalAmount += payment.Amount;
+
+            if (payment.AmountForMeat is not null)
+            {
+                totalAmountForMeat += payment.AmountForMeat.Value;
+            }
+        }
+        
+        decimal toRefund = Math.Ceiling(totalAmount / 3);
+        decimal toRefundForMeat = Math.Ceiling(totalAmountForMeat / 2);
+
+        List<Refund> refunds = [];
+        
+        if (toRefund > 0)
+        {
+            refunds.Add(new Refund("Andrzej", "Natalia and Mikołaj", toRefund));
+        }
+        
+        if (toRefundForMeat > 0)
+        {
+            refunds.Add(new Refund("Andrzej", "Mikołaj", toRefundForMeat));
         }
         
         return refunds;
--- PaymentsSharing/Refunds/RefundsTableViewModel.cs
+++ PaymentsSharing/Refunds/RefundsTableViewModel.cs
@@ -6,9 +6,5 @@ internal class RefundsTableViewModel(Refunds refunds)
 {
     public MonthAndYear MonthAndYear { get; set; } = MonthAndYear.Now;
     
-    public IEnumerable<string> From => refunds.FromMonth(MonthAndYear).Select(refund => refund.From).Distinct();
-    public IEnumerable<string> To => refunds.FromMonth(MonthAndYear).Select(refund => refund.To).Distinct();
-
-    public string ToReturn(string from, string to) =>
-        refunds.FromMonth(MonthAndYear).FirstOrDefault(refund => refund.From == from && refund.To == to)?.Amount.ToString("0 PLN") ?? "-";
+    public IEnumerable<Refund> Refunds => refunds.FromMonth(MonthAndYear);
 }
