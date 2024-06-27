6c272671f2da7be88308af1c4e749360896ffc88
Daniel Kowalski <danielk.biznes@gmail.com>
2024-06-27 21:39:28 +0200

    Add retrieval and handling of PaymentRemoved events
    The code has been updated to not only retrieve and handle `PaymentAdded` events but also `PaymentRemoved` events. This involves getting the `PaymentRemoved` events from the event store and removing appropriate payments based on those events.

--- PaymentsSharing/EventStore/EventsGetter.cs
+++ PaymentsSharing/EventStore/EventsGetter.cs
@@ -13,9 +13,9 @@ internal class EventsGetter(IEventStore eventStore, Payments.Payments payments)
     {
         Console.WriteLine("Getting payments");
 
-        IEnumerable<PaymentAdded?> events = await eventStore.GetEvents<PaymentAdded>();
+        IEnumerable<PaymentAdded?> added = await eventStore.GetEvents<PaymentAdded>();
 
-        foreach (PaymentAdded? @event in events)
+        foreach (PaymentAdded? @event in added)
         {
             if (@event is null)
             {
@@ -27,6 +27,18 @@ internal class EventsGetter(IEventStore eventStore, Payments.Payments payments)
                 @event.Amount,
                 @event.AmountForMeat,
                 @event.Description));
+        }
+        
+        IEnumerable<PaymentRemoved?> removed = await eventStore.GetEvents<PaymentRemoved>();
+        
+        foreach (PaymentRemoved? @event in removed)
+        {
+            if (@event is null)
+            {
+                continue;
+            }
+    
+            payments.Remove(@event.Payment);
         }
         
         Console.WriteLine("Got payments");
